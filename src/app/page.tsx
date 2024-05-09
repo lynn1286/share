98f75d583b1c941d9f5f31b90620b27f8d51da3f
lynn1286 <lynn1286@126.com>
2024-05-09 14:30:07 +0800

    feat: :sparkles: 设置缓存时效

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image'
 
+export const revalidate = 10 // 设置时效
+
 export default async function Home() {
   const { data } = await (
     await fetch('https://api.uomg.com/api/rand.music?sort=热歌榜&format=json')
