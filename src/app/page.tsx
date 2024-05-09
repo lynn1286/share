ac75b7bbc4e445e7ab79a0afa2f3bb5b46f2b485
lynn1286 <lynn1286@126.com>
2024-05-09 18:27:49 +0800

    feat: :sparkles: 多个时效问题

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,49 @@
+import { cookies } from 'next/headers'
 import Image from 'next/image'
 
+// export const revalidate = 3600
+
 async function getData() {
-  // 接口每次调用都会返回一个随机的猫猫图片数据
   const res = await fetch('https://api.thecatapi.com/v1/images/search', {
     next: { revalidate: 10 }
   })
   if (!res.ok) {
     throw new Error('Failed to fetch data')
   }
+  return res.json()
+}
 
+async function getMusicData() {
+  const res = await fetch('https://api.uomg.com/api/rand.music?sort=热歌榜&format=json', {
+    next: { revalidate: 3600 }
+  })
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
   return res.json()
 }
 
 export default async function Page() {
-  const data = await getData()
+  cookies().get('next.js')
 
-  return <Image src={data[0].url} width={300} height={300} alt="" />
+  const data = await getData()
+  const { data: musics } = await getMusicData()
+
+  return (
+    <div>
+      <div>
+        <div>cat</div>
+        <Image src={data[0].url} width={300} height={300} alt="" />
+      </div>
+      <div className="mt-6">
+        <div>music</div>
+        <div>music name： {musics.name}</div>
+        <Image src={musics.picurl} width={300} height={300} alt="" />
+      </div>
+
+      <div className="mt-6">
+        <div>current time： {new Date().toLocaleTimeString()}</div>
+      </div>
+    </div>
+  )
 }
