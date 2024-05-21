f042678bfbf03bd97a5711882c0a79f184e558f4
lynn1286 <lynn1286@126.com>
2024-05-21 10:17:38 +0800

    feat: :sparkles: 图片的一些介绍

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,18 @@ const imageLoader = ({ src, width, quality }: { src: string; width: number; qual
   return `${src}?x-oss-process=image/resize,m_lfit,w_${width}`
 }
 
+const imageqiniuLoader = ({
+  src,
+  width,
+  quality
+}: {
+  src: string
+  width: number
+  quality?: number
+}) => {
+  return `${src}?imageView2/2/w/${width}`
+}
+
 export default function Home() {
   return (
     <div className="flex items-center justify-center mt-56">
@@ -18,6 +30,15 @@ export default function Home() {
         sizes="(min-width: 600px) 2000px, (min-width: 500px) 1000px, 20px"
         alt="responsive image"
         src="https://oss-us-east.mademine.com/mademine/back-end/XHBPJJ0P271X02.jpg"
+      ></Image>
+
+      <Image
+        width={400}
+        height={400}
+        loader={imageqiniuLoader}
+        sizes="(min-width: 600px) 2000px, (min-width: 500px) 1000px, 20px"
+        alt="responsive image"
+        src="https://mademine-user-dev.maiyuan.online/0OtH9aAA44B5EiBCYseTL.jpg"
       ></Image>
 
       <Image
